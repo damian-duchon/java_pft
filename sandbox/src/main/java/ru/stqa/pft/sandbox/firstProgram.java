87c8f4a899bb65b1d4e450f7f22e280ccc1e789b
damian-duchon <34160621+damian-duchon@users.noreply.github.com>
2017-12-05 08:47:00 +0100

    Dodano kilka funkcji

--- sandbox/src/main/java/ru/stqa/pft/sandbox/firstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/firstProgram.java
@@ -3,6 +3,31 @@ package ru.stqa.pft.sandbox;
 public class firstProgram
 {
 	public static void main(String[] args) {
-		System.out.println("Hello, world!");
+		hello("world");
+		hello("user");
+		hello("Damian");
+
+		double l = 5;
+		System.out.println("Powierzchnia kwadratu " + l + " = " + area(l));
+
+		double a = 4;
+		double b = 6;
+		System.out.println("Powierzchnia prostokata " + a + " i " + b + " = " + area(a, b));
 	}
+
+	public static void hello(String somebody) {
+		System.out.println("Hello, " + somebody + "!");
+
+	}
+
+	public static double area(double len) {
+		return len * len;
+	}
+
+	public static double area(double a, double b) {
+		return a * b;
+	}
+
+
+
 }
