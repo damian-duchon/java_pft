0541cdb522df39c80bb209e3ff4f7951a1c8c305
damian-duchon <34160621+damian-duchon@users.noreply.github.com>
2017-12-05 10:57:09 +0100

    Homework 2 part 1,2,3

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -1,19 +1,13 @@
 package ru.stqa.pft.sandbox;
 
 public class Point {
-  private int x;
-  private int y;
+  public double x;
+  public double y;
 
-  public Point(int x, int y) {
+  public Point(double x, double y) {
     this.x = x;
     this.y = y;
   }
 
-  public int getX() {
-    return x;
-  }
 
-  public int getY() {
-    return y;
-  }
 }
--- sandbox/src/main/java/ru/stqa/pft/sandbox/firstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/firstProgram.java
@@ -7,6 +7,11 @@ public class FirstProgram
 		hello("user");
 		hello("Damian");
 
+		Point p1 = new Point(7.1, 5.2);
+		Point p2 = new Point(9.7, 6.0);
+		System.out.println("Odleglosc wynosi " + distance(p1, p2));
+
+
 		Square s = new Square(5);
 		System.out.println("Powierzchnia kwadratu " + s.l + " = " + s.area());
 
@@ -17,6 +22,11 @@ public class FirstProgram
 	public static void hello(String somebody) {
 		System.out.println("Hello, " + somebody + "!");
 
+	}
+  public static double distance(Point p1, Point p2) {
+	   double dx = p1.x - p2.x;
+	   double dy = p1.y - p2.y;
+	   return Math.sqrt(dx * dx + dy * dy);
 	}
 
 
