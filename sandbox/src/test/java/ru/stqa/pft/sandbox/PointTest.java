d48daec02a95e9315dda749b9705dd330cfaa0bd
damian-duchon <34160621+damian-duchon@users.noreply.github.com>
2017-12-12 11:46:38 +0100

    Homework 3 fix

--- sandbox/src/test/java/ru/stqa/pft/sandbox/PointTest.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PointTest.java
@@ -17,6 +17,14 @@ public class PointTest {
     Point p1 = new Point(2.0, 2.0);
     Point p2 = new Point(6.0, 2.0);
     Assert.assertEquals(p1.distance(p2), 4.0);
+  }
+
+
+    @Test
+    public void testPoint2() {
+      Point p1 = new Point(2.0, 2.0);
+      Point p2 = new Point(10.0, 2.0);
+      Assert.assertEquals(p1.distance(p2), 8.0);
 
 }
 }
