67cbd3f0bf06a4ed522495ca7e07a20b905c5f77
damian-duchon <34160621+damian-duchon@users.noreply.github.com>
2018-01-08 13:53:30 +0100

    lesson 4 part9

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
@@ -1,57 +1,45 @@
 package ru.stqa.pft.addressbook.model;
 
 public class GroupData {
-  private final String id;
-  private final String name;
-  private final String header;
-  private final String footer;
+    private int id;
+    private final String name;
+    private final String header;
 
-    public GroupData(String id, String name, String header, String footer) {
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    private final String footer;
+
+    public GroupData(int id, String name, String header, String footer) {
         this.id = id;
         this.name = name;
         this.header = header;
         this.footer = footer;
     }
 
-    public GroupData(String name, String header, String footer) {
-        this.id = null;
+
+       public GroupData(String test1, String name, String header, String footer) {
+        this.id = 0;
         this.name = name;
         this.header = header;
         this.footer = footer;
     }
 
-    public String getId() {
+    public int getId() {
         return id;
     }
 
-  public String getName() {
-    return name;
-  }
-
-  public String getHeader() {
-    return header;
-  }
-
-  public String getFooter() {
-    return footer;
-  }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        GroupData groupData = (GroupData) o;
-
-        if (id != null ? !id.equals(groupData.id) : groupData.id != null) return false;
-        return name != null ? name.equals(groupData.name) : groupData.name == null;
+    public String getName() {
+        return name;
     }
 
-    @Override
-    public int hashCode() {
-        int result = id != null ? id.hashCode() : 0;
-        result = 31 * result + (name != null ? name.hashCode() : 0);
-        return result;
+    public String getHeader() {
+        return header;
+    }
+
+    public String getFooter() {
+        return footer;
     }
 
     @Override
@@ -60,5 +48,24 @@ public class GroupData {
                 "id='" + id + '\'' +
                 ", name='" + name + '\'' +
                 '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        GroupData groupData = (GroupData) o;
+
+        if (id != groupData.id) return false;
+        return name != null ? name.equals(groupData.name) : groupData.name == null;
+
+
+    }
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        return result;
     }
 }
