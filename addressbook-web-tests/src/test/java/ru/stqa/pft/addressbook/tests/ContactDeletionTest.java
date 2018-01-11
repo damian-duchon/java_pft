1d991448cdef81db6236e7f84f53d3f64377906b
damian-duchon <34160621+damian-duchon@users.noreply.github.com>
2018-01-11 12:38:56 +0100

    lesson 5 point 1

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTest.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 public class ContactDeletionTest extends TestBase {
 
-    @Test
+    @Test(enabled = false)
     public void testContactDeletion() {
         app.getNavigationHelper().gotoHomePage();
         if (!app.getContactHelper().isThereAContact()) {
